d41e44a43466223478a22a4f673d77c349c19a4d
rashikolhapure <rashikolhapure@gmail.com>
2024-12-06 19:14:45 +0530

    Implement register flow

---
+++ src/hooks/useRegister.ts
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { Profile } from "../model/Profile";
+import { createProfile } from "../services/auth-service";
+
+export const useRegister = () => {
+  const [error, setError] = useState<string>("");
+  const [isLoading, setLoader] = useState<boolean>(false);
+  const [toast, setToast] = useState<string>("");
+  const signup = (profile: Profile) => {
+    setLoader(true);
+    createProfile(profile)
+      .then((response) => {
+        if (response && response.status === 201) {
+          setToast("Profile is successfully created");
+        }
+      })
+      .catch((error) => setError(error.message))
+      .finally(() => setLoader(false));
+  };
+  return { error, isLoading, signup, toast };
+};
